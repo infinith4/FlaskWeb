aea0120193f1223e36590478689b9a732c57bd6b
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2018-11-10 00:51:34 +0900

    requests api. we can get changed actualHours

--- FlaskWebProject/FlaskWebProject/FlaskWebProject/views.py
+++ FlaskWebProject/FlaskWebProject/FlaskWebProject/views.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
 import requests
+import json
 
 BACKLOG_HOST_GLOBAL = 'https://toyoko.backlog.jp'
 BACKLOG_HOST_IP = 'https://toyoko-ip.backlog.com'
@@ -21,9 +22,36 @@ def home():
     params = {'apiKey': BACKLOG_API_KEY_GLOBAL}
     r = requests.get(BACKLOG_HOST_GLOBAL + '/api/v2/users/myself', params=params)
     print(r.json())
-    params = {'projectId': '39299'}
+
+    params = {'projectKey': 'TYS_SRE', 'apiKey': BACKLOG_API_KEY_GLOBAL}
     r = requests.get(BACKLOG_HOST_GLOBAL + '/api/v2/issues', params=params)
     print(r.json())
+
+    params = {'apiKey': BACKLOG_API_KEY_GLOBAL}
+    r = requests.get(BACKLOG_HOST_GLOBAL + '/api/v2/projects/TYS_SRE', params=params)
+    print(r.json())
+
+    params = {'apiKey': BACKLOG_API_KEY_GLOBAL}
+    r = requests.get(BACKLOG_HOST_GLOBAL + '/api/v2/space')
+    print(r.json())
+
+    issueIdOrKey = 'FFIS_SMAPHOSYS-489'
+    url = BACKLOG_HOST_GLOBAL + '/api/v2/issues/' + issueIdOrKey
+    print(url)
+    params = {'apiKey': BACKLOG_API_KEY_GLOBAL}
+    r = requests.get(url, params=params)
+    print(r.json())
+
+    params = {'apiKey': BACKLOG_API_KEY_GLOBAL}
+    issueIdOrKey = 'FFIS_SMAPHOSYS-489'
+    commentId = '39332511'
+    url = BACKLOG_HOST_GLOBAL + '/api/v2/issues/' + issueIdOrKey + "/comments/" + commentId
+    print(url)
+    r = requests.get(url, params=params)
+    print(r.json())
+    print(json.dumps(r.json(), indent=2))
+    #data = json.loads(r.json())
+    #print(json.dumps(data, indent=2))
 
     """Renders the home page."""
     return render_template(
