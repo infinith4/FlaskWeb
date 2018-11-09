70a8e8bd628c7e3b925569833d3da48ae9c1e520
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2018-11-09 21:37:35 +0900

    add request

--- FlaskWebProject/FlaskWebProject/FlaskWebProject/views.py
+++ FlaskWebProject/FlaskWebProject/FlaskWebProject/views.py
@@ -2,13 +2,29 @@
 Routes and views for the flask application.
 """
 
+# https://backlog.com/developer/applications/
+
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
+import requests
+
+BACKLOG_HOST_GLOBAL = 'https://toyoko.backlog.jp'
+BACKLOG_HOST_IP = 'https://toyoko-ip.backlog.com'
+BACKLOG_API_KEY_GLOBAL = ''
+BACKLOG_API_KEY_IP = ''
 
 @app.route('/')
 @app.route('/home')
+
 def home():
+    params = {'apiKey': BACKLOG_API_KEY_GLOBAL}
+    r = requests.get(BACKLOG_HOST_GLOBAL + '/api/v2/users/myself', params=params)
+    print(r.json())
+    params = {'projectId': '39299'}
+    r = requests.get(BACKLOG_HOST_GLOBAL + '/api/v2/issues', params=params)
+    print(r.json())
+
     """Renders the home page."""
     return render_template(
         'index.html',
